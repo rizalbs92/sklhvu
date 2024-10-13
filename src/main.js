e7c29a5bf1520ea1409ec8b921ad89b797842385
rizalbs92 <a.r.syaifulloh@gmail.com>
2024-10-13 20:00:45 +0700

    add folder

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+//import router
+import router from './router/Index'
+
+const app = createApp(App)
+
+//gunakan router di vue js dengan plugin "use"
+app.use(router)
+
+//mount
+app.mount('#app')
